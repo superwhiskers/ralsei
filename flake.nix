868d17086382a31ec58311c1ad38a98f0fa6906b
superwhiskers <whiskerdev@protonmail.com>
2021-05-29 17:07:28 -0500

    ralsei: add fixed nn-curl and update the flake to contain libxml2

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
       let pkgs = nixpkgs.legacyPackages."${system}";
       in {
         devShell = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ openssl pkg-config rustup ];
+          nativeBuildInputs = with pkgs; [ openssl pkg-config rustup libxml2 ];
         };
       });
 }
